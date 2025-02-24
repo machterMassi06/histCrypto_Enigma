b3e9abe01d1faec86d4f26486963f1bc68f45f86
Massinissa MACHTER <massinissa.machter@gmail.com>
2025-02-24 18:16:43 +0100

    implementation of caesar cipher

--- src/main/java/com/mycrypto/cipher/CaesarCipher.java
+++ src/main/java/com/mycrypto/cipher/CaesarCipher.java
@@ -1,4 +1,33 @@
 package com.mycrypto.cipher;
 
+import com.mycrypto.utils.Utils;
+
 public class CaesarCipher {
+    // Encrypt text or file content using the Caesar cipher
+    public static String encrypt(String input , int key,boolean isFilePath){
+        String text = isFilePath? Utils.readFile(input) : input;
+        return process(text,key);
+    }
+
+    // Decrypt text or file content using the Caesar cipher
+    public static String decrypt(String input , int key,boolean isFilePath){
+        String text = isFilePath? Utils.readFile(input) : input;
+        return process(text,key-(key%26));// Reverse shift for decryption
+    }
+
+    // General fct to process text for encryption/decryption
+    private static String process(String text,int key){
+        StringBuilder result = new StringBuilder();
+        key = key % 26; // Keep key within valid range
+
+        for (char c :text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char base = Character.isUpperCase(c) ? 'A' : 'a';
+                result.append((char) ((c - base + key) % 26 + base));
+            } else {
+                result.append(c); // Keep non-alphabetic(blanc)characters unchanged
+            }
+        }
+        return result.toString();
+    }
 }
