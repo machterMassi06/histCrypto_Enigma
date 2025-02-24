7772cccba6779ea0b89c5423b1118cda4b0295bd
Massinissa MACHTER <massinissa.machter@gmail.com>
2025-02-24 18:18:00 +0100

    Entry point of programme (first version)

--- src/main/java/com/mycrypto/main/Main.java
+++ src/main/java/com/mycrypto/main/Main.java
@@ -1,7 +1,11 @@
 package com.mycrypto.main;
-
+import com.mycrypto.cipher.CaesarCipher;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String text = "HELLO WORLD";
+        int key =3;
+        String encryptedText = CaesarCipher.encrypt(text, key, false);
+        System.out.println("key= "+key+" encrypted text = "+encryptedText);
+        System.out.println("origin text= "+CaesarCipher.decrypt(text, key, false));
     }
 }
