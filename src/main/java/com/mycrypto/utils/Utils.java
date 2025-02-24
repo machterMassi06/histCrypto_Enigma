3a32b6a18828f0821da9a3a07a46f2e68bb5395b
Massinissa MACHTER <massinissa.machter@gmail.com>
2025-02-24 18:17:28 +0100

    utility functions

--- src/main/java/com/mycrypto/utils/Utils.java
+++ src/main/java/com/mycrypto/utils/Utils.java
@@ -1,4 +1,26 @@
 package com.mycrypto.utils;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
 
-public class Utils {
+public class Utils{
+
+    public static String readFile(String filePath){
+        try {
+            return Files.readString(Path.of(filePath));
+        }catch (IOException e){
+            System.err.println("Error reading file: " + e.getMessage());
+            return "";
+        }
+    }
+
+    public static void writeFile(String filePath , String content){
+        try {
+            Files.writeString(Path.of(filePath), content);
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
 }
+
+
